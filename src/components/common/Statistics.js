0c3437dc6ea4012349e6c5b27e92d079f739dab4
chaeyeonLee <dmsgp62@gmail.com>
2021-11-05 18:12:34 +0900

    chart에 수출액 필터해서 표현구현/ 수입, 수출 총 금액 표시

--- src/components/common/Statistics.js
+++ src/components/common/Statistics.js
@@ -1,45 +1,67 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { store } from "../../App";
 import { PieChart } from "react-minimal-pie-chart";
 import style from "../../assets/css/statistics.module.css";
 
 // https://www.npmjs.com/package/react-minimal-pie-chart
 function Statistics() {
-  const [state, dispatch] = useContext(store);
+  const [state] = useContext(store);
+  const [income, setIncome] = useState(0);
+  const [cost, setCost] = useState(0);
 
-  const Element = (props) => {
-    return (
-      <text
-        dominant-baseline="central"
-        x="50"
-        y="50"
-        dx="19.151111077974452"
-        dy="16.06969024216348"
-        text-anchor="middle"
-        style={{ fontSize: "10px" }}
-      >
-        <tspan x="50" dy="1.2em">
-          {`${Math.round(props.percentage)} %`}
-        </tspan>
-        <tspan x="0" dy="1.2em">
-          {props.title}
-        </tspan>
-      </text>
-    );
-  };
+  // 지출
+  const date = [
+    { value: 0, color: "#9b1b30", title: "food" },
+    { value: 0, color: "#5a5b9f", title: "shopping" },
+    { value: 0, color: "#e2583e", title: "transportation" },
+    { value: 0, color: "#955251", title: "coffee" },
+    { value: 0, color: "#c74375", title: "date" },
+    { value: 0, color: "#53b0ae", title: "cluture" },
+    { value: 0, color: "#88b04b", title: "event" },
+    { value: 0, color: "#5f4b8b", title: "selfImprovement" },
+    { value: 0, color: "#0c4a86", title: "exercise" },
+    { value: 0, color: "#7d8f96", title: "etc" },
+  ];
+  // 수입
+  const dateIncome = { value: 0 };
+
+  state.money_list &&
+    state.money_list.map((val) => {
+      if (val.spending === "지출") {
+        const idx = date.findIndex((v) => v.title === val.kinds);
+        date[idx].value += Number(val.price);
+      } else {
+        dateIncome.value += Number(val.price);
+      }
+    });
+
+  useEffect(() => {
+    if (state.money_list) {
+      setCost(date.reduce((sum, item) => sum + Number(item.value), 0));
+      setIncome(dateIncome.value);
+    }
+  }, []);
 
   return (
     <div className={style["statistics_container"]}>
-      <p>ddd</p>
-      지출 수입
-      <PieChart
-        data={[
-          { title: "Excellent", value: 10, color: "#8dcd81" },
-          { title: "Good", value: 15, color: "#eefa6b" },
-          { title: "Weak", value: 20, color: "#FF6382" },
-        ]}
-        label={({ dataEntry }) => <Element {...dataEntry} />}
-      />
+      <div className="left">
+        <h2>
+          <span>지출금</span>
+          {cost}
+        </h2>
+        <h4>
+          <span>수입금</span>
+          {income}
+        </h4>
+      </div>
+      <div className="right">
+        <PieChart
+          data={date}
+          label={(props) => {
+            return props.dataEntry.title;
+          }}
+        />
+      </div>
     </div>
   );
 }
